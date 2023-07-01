c671864bba0119e9df4d1960f1b97c8fcfd6ce29
Samusarand <sandrazuniga.dg@gmail.com>
2023-06-30 21:00:40 -0400

    agregando la paginacion

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-10.times do |i|
+20.times do |i|
     @tweets = Tweet.create(tweet: "Tweet #{i}",
    username: "username#{i}tweet")
 
